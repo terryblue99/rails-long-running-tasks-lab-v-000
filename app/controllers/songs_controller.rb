69badcf9b723d0a43415207c342fb1f2e44f02db
Terry Blue <terryblue@comcast.net>
2018-04-03 18:24:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
 
   def upload
     binding.pry
-    CSV.foreach(params['file'].path, headers: true) do |song|
+    CSV.foreach(params[:file].path, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
     end
     redirect_to songs_path
